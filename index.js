561ebc8f3efcdff8238c0c4494d9c60dfd450a61
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-04 17:12:40 -0300

    first attempt at a tick based animation for the road

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const { Application } = require('pixi.js');
+const pull = require('pull-stream')
+const tickThrough = require('pull-pixi-tick')
 
 const styles = require('./src/styles');
 const buildRoad = require('./src/road');
 
-const { view, stage, renderer } = new Application();
+const { view, stage, renderer, ticker } = new Application();
 view.className = styles.canvas;
+
+const sink = sprite => read => {
+    const next = (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        sprite.y = data;
+        read(null, next);
+    }
+    read(null, next);
+}
 
 const init = () => {
     document.body.appendChild(view);
-    buildRoad(renderer, 1000, road => {
+    console.log(view.offsetHeight)
+    buildRoad(renderer, view.offsetHeight, road => {
+        pull(
+            pull.count(310),
+            tickThrough(ticker),
+            sink(road)
+        )
         stage.addChild(road);
     });
 };
