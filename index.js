beba88a049295d24d14df38c3c3fb6d121688ac8
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-04 19:52:59 -0300

    simple animation of the road using ticker as the source stream and a sink that moves the straight road

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Application } = require('pixi.js');
 const pull = require('pull-stream')
-const tickThrough = require('pull-pixi-tick')
+const tickSource = require('pull-pixi-tick').source
 
 const styles = require('./src/styles');
 const buildRoad = require('./src/road');
@@ -8,24 +8,25 @@ const buildRoad = require('./src/road');
 const { view, stage, renderer, ticker } = new Application();
 view.className = styles.canvas;
 
+const speed = 6.4;
 const sink = sprite => read => {
-    const next = (err, data) => {
+    const next = (err, deltaTime) => {
         if (err) {
             return console.log(err)
         }
-        sprite.y = data;
+        sprite.y = -128 + (sprite.y + deltaTime * speed) % 128;
         read(null, next);
     }
     read(null, next);
 }
 
 const init = () => {
+    renderer.roundPixels = true
     document.body.appendChild(view);
     console.log(view.offsetHeight)
-    buildRoad(renderer, view.offsetHeight, road => {
+    buildRoad(renderer, view.offsetHeight + 128 * 2, road => {
         pull(
-            pull.count(310),
-            tickThrough(ticker),
+            tickSource(ticker),
             sink(road)
         )
         stage.addChild(road);
