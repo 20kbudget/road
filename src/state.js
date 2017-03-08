55bcbda41a9ce35e7bed1deaf0fdf05d56aba3bb
Fabricio C Zuardi <fabricio@fabricio.org>
2017-03-08 19:15:29 -0300

    improved readability in state file

--- src/state.js
+++ src/state.js
@@ -1,41 +1,44 @@
+const extend = require('xtend');
 const nanobus = require('nanobus');
 const Stats = require('stats.js');
 const { Container } = require('pixi.js');
 const { autoDetectRenderer } = require('./custompixi');
 const classNames = require('./styles');
 
-const framerate = 40;
-const debug = true;
-const paused = false;
-const dimensions = [384, 640];
-const rendererOptions = {
-    autoResize: true,
-    forceFXAA: true,
-    roundPixels: true,
-    backgroundColor: 0x111111
-};
-const textures = {
-    playIcon: {
-        url: './assets/right.png',
-        size: 15312
+const baseState = {
+    framerate: 40,
+    debug: true,
+    paused: false,
+    dimensions: [384, 640],
+    rendererOptions: {
+        autoResize: true,
+        forceFXAA: true,
+        roundPixels: true,
+        backgroundColor: 0x111111
     },
-    pauseIcon: {
-        url: './assets/pause.png',
-        size: 15197
+    textures: {
+        // playIcon: {
+            // url: './assets/right.png',
+            // size: 15312
+        // },
+        // pauseIcon: {
+            // url: './assets/pause.png',
+            // size: 15197
+        // },
+        playerCar: {
+            url: './assets/car_red_small_4.png',
+            size: 703
+        }
     },
-    playerCar: {
-        url: './assets/car_red_small_4.png',
-        size: 703
+    loading: {
+        total: 0,
+        current: 0
     }
-};
-const loading = {
-    total: 0,
-    current: 0
 };
 
 let initialState = () => {
     const bus = nanobus();
-    const renderer = autoDetectRenderer(...dimensions, ...rendererOptions);
+    const renderer = autoDetectRenderer(...baseState.dimensions, ...baseState.rendererOptions);
     const { view } = renderer;
     const stage = new Container();
     const branch = (flag, a, b) => flag ? a : b;
@@ -57,12 +60,7 @@ let initialState = () => {
     const debugDiv = stats.dom;
     stats.showPanel(0);
 
-    const state = {
-        framerate,
-        debug,
-        paused,
-        classNames,
-        textures,
+    const state = extend(baseState, {
         bus,
         renderer,
         view,
@@ -70,9 +68,8 @@ let initialState = () => {
         draw,
         debugDiv,
         tick,
-        loading,
-        sprites: {}
-    };
+        classNames
+    });
 
     return state;
 };
